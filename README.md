fe659ee2c5f4511c5f7af4614d0f63ac8e387a70
Dāvis Krēgers <daviskregers@protonmail.com>
2022-02-12 08:51:57 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# archlinux-install
-An archlinux install script spit away from https://github.com/daviskregers/old-dotfiles
+# Archlinux install
+
+My flimsy scripts for installing archlinux https://github.com/daviskregers/old-dotfiles/tree/v4/install/archlinux
+This version has cut off the dotfiles installation part though.
+
+- The `./install-arch.sh` script is for running into the archlinux installation boot
+- The `./configure-arch.sh` script is for running after the installation, while in `arch-chroot /mnt`.
+
+### Using pacman proxy
+
+The script will automatically set up pacman mirrors, if you wish to use your own proxy, set an environment variable.
+
+```
+export ARCHLINUX_PROXY_HOST=http://192.168.1.X:8099
+```
+
+### Dualbooting systemd-boot
+
+If you want to dualboot into windows using systemd-boot, you can use these steps:
+
+```
+lsblk -- find the windows EFI partition (should be 100MB size)
+sudo mount /dev/sdc2 /mnt
+cd /mnt/EFI
+sudo cp -ax Microsoft /boot/EFI
+```
+
+https://github.com/spxak1/weywot/blob/main/Pop_OS_Dual_Boot.md#22-how-to-add-an-option-for-windows-in-pop_os-boot-menu
---
+++ configure-arch.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+source "../../.bash_helpers"
+source .install_helpers
+
+PWD=$(pwd)
+DOTFILES=$(dirname $(dirname $PWD))
+[ -d /sys/firmware/efi ] && TPE="EFI" || TPE="BIOS"
+
+if [ $(uname -n ) != "archiso" ]; then
+	echo -e "${BLINK}This isn't an arch installation, exiting...$END"
+	exit 1;
+fi
+
+echo -e "${GREEN}Arch configuration $END"
+echo -e $SEPARATOR
+
+echo -e "Change root password"
+passwd
+
+echo -e "Set locale & timezone"
+
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+LOCALE=en_US.UTF-8
+echo LANG=$LOCALE > /etc/locale.conf
+export LANG=$LOCALE
+locale-gen --purge $LOCALE
+
+ln -sf /usr/share/zoneinfo/Europe/Riga /etc/localtime
+hwclock --systohc --utc
+
+read -p "What will your machine hostname be? " hostname
+echo $hostname > /etc/hostname
+cat /etc/hostname
+
+echo "Set up hosts"
+echo "127.0.0.1 	localhost	$hostname" >> /etc/hosts
+echo "::1		localhost	$hostname" >> /etc/hosts
+cat /etc/hosts
+
+echo "Set up bootloader"
+device=$(get_device)
+prefix=$(partition_prefix $device)
+
+systempartition=${device}${prefix}2
+efipartition=${device}${prefix}1
+
+mkinitcpio -P
+systemd-machine-id-setup
+uuid=$(blkid --match-tag PARTUUID -o value $systempartition)
+echo "GOT UUID: $uuid from $systempartition"
+
+if [ $TPE == "EFI" ]; then
+echo "EFI bootloader"
+
+pacman -S --noconfirm efibootmgr
+sed -i 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)/g' /etc/mkinitcpio.conf
+
+mkinitcpio -p linux
+bootctl --path=/boot install
+
+cat <<EOF >/boot/loader/entries/arch.conf
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options cryptdevice=PARTUUID=${uuid}:system root=/dev/mapper/system rw
+EOF
+
+cat <<EOF >/boot/loader/loader.conf
+default arch
+timeout 3
+EOF
+
+else
+echo "Installing GRUB"
+
+pacman –S grub os-prober
+cat <<EOF >/etc/default/grub
+GRUB_CMDLINE_LINUX="cryptdevice=PARTUUID=${uuid}:system root=/dev/mapper/system"
+GRUB_ENABLE_CRYPTODISK=y
+GRUB_PRELOAD_MODULES="luks cryptodisk lvm ext2"
+GRUB_DISABLE_OS_PROBER=false
+EOF
+grub-install $device
+grub-mkconfig -o /boot/grub/grub.cfg
+
+fi
+
+echo -e "${RED}Generating new user$END"
+
+read -p "Enter your user name: " username
+useradd -m $username
+passwd $username
+usermod -a -G wheel $username
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
+cat /etc/sudoers | grep wheel
+
+echo -e "${RED}Setting up NetworkManager$END"
+
+pacman -S networkmanager
+systemctl enable NetworkManager
+
+setup_mirrors
+
+echo -e "${GREEN}Everything should be done, you can exit and reboot now $END"
